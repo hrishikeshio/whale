691b0c99c0f3b48070992b63e82cdc526596db27
Frans Slothouber <frans.slothouber@gmail.com>
2013-02-23 09:07:21 +0100

    Reads an aiff file

--- FFT/fft.c
+++ FFT/fft.c
@@ -1,24 +1,23 @@
 
+/*
+ * requires
+ *    libsndfile
+ *    libfftw3
+ *
+ *     sudo apt-get install libfftw3-doc 
+ *                  libfftw3-3 libfftw3-bin
+ *                  libfftw3-dev
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <fftw3.h>
-
-void transform( void );
-
+#include <sndfile.h>
 
 
 double frand ( void )
 {
     return ( ( double ) rand ( ) / ( RAND_MAX ) );
-}
-
-
-
-int main( int argc, char** argv )
-{
-    transform();
-
-    return EXIT_SUCCESS;
 }
 
 
@@ -45,7 +44,35 @@ void transform( void )
 
     plan_forward = fftw_plan_dft_r2c_1d ( n, in, out, FFTW_ESTIMATE );
     fftw_execute ( plan_forward );
-
 }
 
+
+void test_read( void )
+{
+    SF_INFO a_sf_info;
+    SNDFILE* sf = NULL;
+    double *in;
+    int n = 2000;
+
+    sf_count_t n_to_read = n;
+    sf_count_t n_read = 0;
+
+    in = fftw_malloc ( sizeof ( double ) * n );
+    sf = sf_open( "train19050.aiff", SFM_READ, &a_sf_info );
+
+    n_read = sf_read_double( sf, in, n_to_read );
+
+    for ( int i = 0; i < n; ++i ) {
+        printf( "%d %f\n", i, in[i] );
+    }
+    printf( "Read %d samples\n", n_read );
+}
+
+
+int main( int argc, char** argv )
+{
+    test_read();
+    transform();
+    return EXIT_SUCCESS;
+}
 
